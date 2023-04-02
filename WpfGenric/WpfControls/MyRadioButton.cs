5f6b2e2658e7dd879ec168f7a81f362d3671e973
KaburiCorder <specialemail90@gmail.com>
2023-04-02 23:21:31 +0900

    Themes/Generic.xaml 강의 자료

---
+++ WpfGenric/WpfControls/MyRadioButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControls
+{
+  public class MyRadioButton : RadioButton
+  {
+    static MyRadioButton()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(MyRadioButton),
+        new FrameworkPropertyMetadata(typeof(MyRadioButton)));
+    }
+  }
+}
